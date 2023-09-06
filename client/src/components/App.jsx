ba88dbdd6a8789ea19fec0465ce0b4a4d2e1359c
Fahim Ahmed <p2595607@my365.dmu.ac.uk>
2023-09-06 18:50:05 +0100

    Designed a homepage on client

---
+++ client/src/components/App.jsx
@@ -0,0 +1,43 @@
+import '../main.css';
+import reactLogo from '../assets/react.svg';
+import { useState, useEffect } from 'react';
+import { HashRouter, Routes, Route, Link } from 'react-router-dom';
+import HomePage from './HomePage';
+
+function App() {
+  const [apiResponse, setApiResponse] = useState('');
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    fetch('http://localhost:9000/api')
+      .then(res => res.text())
+      .then(res => setApiResponse(res))
+      .catch(err => console.log(err));
+  }, []);
+
+  return (
+    <>
+    <HashRouter basename='/'>
+      <div className='menu-bar'>
+        <h1>Latest Products Blog</h1>
+        <Link to='/'>
+          <button type='button' className='menu'>Home</button>
+        </Link>
+        <Link to='/sign-up'>
+          <button type='button' className='menu'>Sign up</button>
+        </Link>
+        <Link to='/log-in'>
+          <button type='button' className='menu'>Login</button>
+        </Link>
+      </div>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+      </Routes>
+      <p>{apiResponse}</p>
+    </HashRouter>
+    <footer>Developed by Fahim Ahmed <img src={reactLogo} /></footer>
+    </>
+  )
+}
+
+export default App
---
+++ client/src/components/HomePage.jsx
@@ -0,0 +1,15 @@
+import { Link } from 'react-router-dom';
+
+const HomePage = () => {
+    return (
+        <div className='home-page'>
+            <div className='information'>
+                <h2>Welcome to my blog</h2>
+                <p>Here, we discuss any kind of latest products, whether it is about technology or fashion</p>
+                <p>Please login or sign up if you are a new user</p>
+            </div>
+        </div>
+    );
+};
+
+export default HomePage;
