104de87dd2a0b99bd225750d7efc5fecfbf7133f
Fahim Ahmed <p2595607@my365.dmu.ac.uk>
2023-09-14 20:05:00 +0100

    Implemented delete post

--- client/src/components/ViewPost.jsx
+++ client/src/components/ViewPost.jsx
@@ -6,6 +6,7 @@ const ViewPost = () => {
     const [post, setPost] = useState();
     const [comments, setComments] = useState();
     const [comment, setComment] = useState("");
+
     const navigate = useNavigate();
     
     const user = localStorage.getItem("id");
@@ -48,13 +49,32 @@ const ViewPost = () => {
         localStorage.removeItem("user");
     }
 
+    function checkUser() {
+        return user === post.user._id;
+    }
+
     function handleText(e) {
         setComment(e.target.value);
     }
 
+    function handleDelete() {
+        fetch("http://localhost:9000/api/posts/delete", {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ post: post._id })
+        })
+        .catch((err) => { 
+            console.log(err);
+        })
+        .finally(navigate("/posts"));
+    }
+
     function handleSubmit() {
-        setLoading(true);
         if (comment.length > 0) {
+            setLoading(true);
             fetch("http://localhost:9000/api/posts/comments/create", {
                 method: 'POST',
                 headers: {
@@ -85,7 +105,7 @@ const ViewPost = () => {
             })
             .finally(() => {
                 if (comments.length > 0) { 
-                    setLoading(false) ;
+                    setLoading(false);
                 }
             });
         }
@@ -100,85 +120,170 @@ const ViewPost = () => {
 
     } else if (getUser() !== `${undefined}` && getUser() !== `${null}` && post !== undefined) {
         if (comments.length > 0) {
-            return(
-                <>
-                    <div className="menu-bar">
-                        <h1>Latest Products Blog</h1>
-                        <Link to="/">
-                            <button type="button" className="menu" onClick={logout}>Logout</button>
-                        </Link>
-                        <Link to="/posts">
-                            <button type="button" className="menu">Back to posts</button>
-                        </Link>
-                    </div>
-                    <div className="single-post">
-                        <h1>Title: {post.title}</h1>
-                        <h3>Posted by {post.user.username}</h3>
-                        <p>{post.text}</p>
-                    </div>
-                    <div className="comments-section">
-                        <div className="comment-form">
-                            <div className="forms" onSubmit={() => setKey(currentKey => currentKey + 1)}>
-                                <label>
-                                    New comment:
-                                </label>
-                                <textarea name="new-comment"
-                                    cols={30}
-                                    rows={10}
-                                    maxLength={1000}
-                                    onChange={handleText}
-                                />
-                                <button type="button" onClick={handleSubmit}>Submit</button>
-                            </div>
+            if (checkUser()) {
+                return(
+                    <>
+                        <div className="menu-bar">
+                            <h1>Latest Products Blog</h1>
+                            <Link to="/">
+                                <button type="button" className="menu" onClick={logout}>Logout</button>
+                            </Link>
+                            <Link to="/posts">
+                                <button type="button" className="menu">Back to posts</button>
+                            </Link>
                         </div>
-                        <h2>Comments</h2>
-                        {comments.map((comment) => (
-                            <div key={comment._id} className="comment">
-                                <p>{comment.text}</p>
-                                <p>by {comment.user.username}</p>
-                                <p>Date: {comment.timestamp.substr(0, 10)}</p>
+                        <div className="single-post">
+                            <h1>Title: {post.title}</h1>
+                            <h3>Posted by {post.user.username}</h3>
+                            <p>{post.text}</p>
+                            <button type="button" onClick={handleDelete}>Delete post</button>
+                        </div>
+                        <div className="comments-section">
+                            <div className="comment-form">
+                                <div className="forms">
+                                    <label>
+                                        New comment:
+                                    </label>
+                                    <textarea name="new-comment"
+                                        cols={30}
+                                        rows={10}
+                                        maxLength={1000}
+                                        onChange={handleText}
+                                    />
+                                    <button type="button" onClick={handleSubmit}>Submit</button>
+                                </div>
                             </div>
-                        ))}
-                    </div>
-                </>
-            );
+                            <h2>Comments</h2>
+                            {comments.map((comment) => (
+                                <div key={comment._id} className="comment">
+                                    <p>{comment.text}</p>
+                                    <p>by {comment.user.username}</p>
+                                    <p>Date: {comment.timestamp.substr(0, 10)}</p>
+                                </div>
+                            ))}
+                        </div>
+                    </>
+                );
+            } else {
+                return(
+                    <>
+                        <div className="menu-bar">
+                            <h1>Latest Products Blog</h1>
+                            <Link to="/">
+                                <button type="button" className="menu" onClick={logout}>Logout</button>
+                            </Link>
+                            <Link to="/posts">
+                                <button type="button" className="menu">Back to posts</button>
+                            </Link>
+                        </div>
+                        <div className="single-post">
+                            <h1>Title: {post.title}</h1>
+                            <h3>Posted by {post.user.username}</h3>
+                            <p>{post.text}</p>
+                        </div>
+                        <div className="comments-section">
+                            <div className="comment-form">
+                                <div className="forms">
+                                    <label>
+                                        New comment:
+                                    </label>
+                                    <textarea name="new-comment"
+                                        cols={30}
+                                        rows={10}
+                                        maxLength={1000}
+                                        onChange={handleText}
+                                    />
+                                    <button type="button" onClick={handleSubmit}>Submit</button>
+                                </div>
+                            </div>
+                            <h2>Comments</h2>
+                            {comments.map((comment) => (
+                                <div key={comment._id} className="comment">
+                                    <p>{comment.text}</p>
+                                    <p>by {comment.user.username}</p>
+                                    <p>Date: {comment.timestamp.substr(0, 10)}</p>
+                                </div>
+                            ))}
+                        </div>
+                    </>
+                );
+            }
         } else {
-            return(
-                <>
-                    <div className="menu-bar">
-                        <h1>Latest Products Blog</h1>
-                        <Link to="/">
-                            <button type="button" className="menu" onClick={logout}>Logout</button>
-                        </Link>
-                        <Link to="/posts">
-                            <button type="button" className="menu">Back to posts</button>
-                        </Link>
-                    </div>
-                    <div className="single-post">
-                        <h1>Title: {post.title}</h1>
-                        <h3>Posted by {post.user.username}</h3>
-                        <p>{post.text}</p>
-                    </div>
-                    <div className="comments-section">
-                        <div className="comment-form">
-                            <div className="forms">
-                                <label>
-                                    New comment:
-                                </label>
-                                <textarea name="new-comment"
-                                    cols={30}
-                                    rows={10}
-                                    maxLength={1000}
-                                />
-                                <button type="button">Submit</button>
-                            </div>
+            if (checkUser()) {
+                return(
+                    <>
+                        <div className="menu-bar">
+                            <h1>Latest Products Blog</h1>
+                            <Link to="/">
+                                <button type="button" className="menu" onClick={logout}>Logout</button>
+                            </Link>
+                            <Link to="/posts">
+                                <button type="button" className="menu">Back to posts</button>
+                            </Link>
                         </div>
-                        <h2>Comments</h2>
-                    </div>
-                </>
-            );
+                        <div className="single-post">
+                            <h1>Title: {post.title}</h1>
+                            <h3>Posted by {post.user.username}</h3>
+                            <p>{post.text}</p>
+                            <button type="button" onClick={handleDelete}>Delete post</button>
+                        </div>
+                        <div className="comments-section">
+                            <div className="comment-form">
+                                <div className="forms">
+                                    <label>
+                                        New comment:
+                                    </label>
+                                    <textarea name="new-comment"
+                                        cols={30}
+                                        rows={10}
+                                        maxLength={1000}
+                                        onChange={handleText}
+                                    />
+                                    <button type="button" onClick={handleSubmit}>Submit</button>
+                                </div>
+                            </div>
+                            <h2>Comments</h2>
+                        </div>
+                    </>
+                );
+            } else {
+                return(
+                    <>
+                        <div className="menu-bar">
+                            <h1>Latest Products Blog</h1>
+                            <Link to="/">
+                                <button type="button" className="menu" onClick={logout}>Logout</button>
+                            </Link>
+                            <Link to="/posts">
+                                <button type="button" className="menu">Back to posts</button>
+                            </Link>
+                        </div>
+                        <div className="single-post">
+                            <h1>Title: {post.title}</h1>
+                            <h3>Posted by {post.user.username}</h3>
+                            <p>{post.text}</p>
+                        </div>
+                        <div className="comments-section">
+                            <div className="comment-form">
+                                <div className="forms">
+                                    <label>
+                                        New comment:
+                                    </label>
+                                    <textarea name="new-comment"
+                                        cols={30}
+                                        rows={10}
+                                        maxLength={1000}
+                                        onChange={handleText}
+                                    />
+                                    <button type="button" onClick={handleSubmit}>Submit</button>
+                                </div>
+                            </div>
+                            <h2>Comments</h2>
+                        </div>
+                    </>
+                );
+            }
         }
-        
 
     } else {
         return(
